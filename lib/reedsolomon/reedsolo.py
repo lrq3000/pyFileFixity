lrq3000/pyFileFixity

commit 0cfd5c89afc27c3a306e5421908c6c09ed3e7c5a

add: more optimization to rs libs
Signed-off-by: Stephen L. <lrq3000@gmail.com>

lib/reedsolomon/reedsolo.py
@@ -123,7 +123,7 @@ def init_tables_base3():
         45, 119, 153, 176, 203, 70, 202, 69, 207, 74, 222, 121, 139, 134,
         145, 168, 227, 62, 66, 198, 81, 243, 14, 18, 54, 90, 238, 41, 123,
         141, 140, 143, 138, 133, 148, 167, 242, 13, 23, 57, 75, 221, 124,
-        132, 151, 162, 253, 28, 36, 108, 180, 199, 82, 246, 1]
+        132, 151, 162, 253, 28, 36, 108, 180, 199, 82, 246, 1] * 2
 
     # Logarithm table, base 3
     gf_log = [None, 0, 25, 1, 50, 2, 26, 198, 75, 199, 27, 104, 51, 238, 223,
@@ -221,16 +221,15 @@ def rs_generator_poly_base3(nsize, fcr=0):
     return g_all
 
 def rs_encode_msg(msg_in, nsym, fcr=0, gen=None):
-    if len(msg_in) + nsym > 255:
-        raise ValueError("message too long")
+    if len(msg_in) + nsym > 255: raise ValueError("message too long")
     if gen is None: gen = rs_generator_poly(nsym, fcr)
     msg_out = bytearray(len(msg_in) + nsym)
     msg_out[:len(msg_in)] = msg_in
     for i in range(0, len(msg_in)):
         coef = msg_out[i]
-        if coef != 0:
+        if coef != 0: # optimization, avoid computing something useless
             for j in range(0, len(gen)):
-                msg_out[i + j] ^= gf_mul(gen[j], coef)
+                if gen[j] != 0: msg_out[i + j] ^= gf_exp[(gf_log[gen[j]] + gf_log[coef])] # optimization, equivalent to gf_mul(gen[j], coef)
     msg_out[:len(msg_in)] = msg_in
     return msg_out
 

lib/brownanrs/polynomial.py
@@ -131,9 +131,9 @@ class Polynomial(object):
         msg_out[:len(dividend)] = dividend
         for i in range(0, len(dividend)):
             coef = msg_out[i]
-            if coef != 0:
+            if coef != 0: # optimization, avoid computing something useless
                 for j in range(0, len(divisor)):
-                    msg_out[i + j] += divisor[j] * coef
+                    if divisor[j] != 0: msg_out[i + j] += divisor[j] * coef
         return Polynomial(msg_out[len(dividend):])
 
     def __divmod__(dividend, divisor):

commit 605876d29cd9ee8f23f67f5715459e3efe7e60f1

fix: incompatibility regression in reedsolo introduced in commit 0cfd5c89afc27c3a306e5421908c6c09ed3e7c5a
Signed-off-by: Stephen L. <lrq3000@gmail.com>

lib/reedsolomon/reedsolo.py
@@ -123,7 +123,7 @@ def init_tables_base3():
         45, 119, 153, 176, 203, 70, 202, 69, 207, 74, 222, 121, 139, 134,
         145, 168, 227, 62, 66, 198, 81, 243, 14, 18, 54, 90, 238, 41, 123,
         141, 140, 143, 138, 133, 148, 167, 242, 13, 23, 57, 75, 221, 124,
-        132, 151, 162, 253, 28, 36, 108, 180, 199, 82, 246, 1] * 2
+        132, 151, 162, 253, 28, 36, 108, 180, 199, 82, 246] * 2 + [1]
 
     # Logarithm table, base 3
     gf_log = [None, 0, 25, 1, 50, 2, 26, 198, 75, 199, 27, 104, 51, 238, 223,
@@ -229,7 +229,7 @@ def rs_encode_msg(msg_in, nsym, fcr=0, gen=None):
         coef = msg_out[i]
         if coef != 0: # optimization, avoid computing something useless
             for j in range(0, len(gen)):
-                if gen[j] != 0: msg_out[i + j] ^= gf_exp[(gf_log[gen[j]] + gf_log[coef])] # optimization, equivalent to gf_mul(gen[j], coef)
+                if gen[j] != 0: msg_out[i + j] ^= gf_exp[gf_log[gen[j]] + gf_log[coef]] # optimization, equivalent to gf_mul(gen[j], coef)
     msg_out[:len(msg_in)] = msg_in
     return msg_out
 