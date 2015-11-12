94c8c82e9f1162a40947449c42a40d492278a629
Stephen L <lrq3000@gmail.com>
2015-11-12 04:06:55 +0100

    silent tests
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- tests/test_rfigc.py
+++ tests/test_rfigc.py
@@ -13,8 +13,8 @@ def test_one_file():
     filein = path_input('tux.jpg')
     fileout = path_output('d.csv')
     fileres = path_results('test_rfigc_test_one_file1.csv')
-    rfigc.main('-i "%s" -d "%s" -g -f' % (filein, fileout))
-    rfigc.main('-i "%s" -d "%s" -f' % (filein, fileout))
+    rfigc.main('-i "%s" -d "%s" -g -f --silent' % (filein, fileout))
+    rfigc.main('-i "%s" -d "%s" -f --silent' % (filein, fileout))
     assert check_eq_files(fileout, fileres)
 
 def test_dir():
@@ -22,6 +22,6 @@ def test_dir():
     filein = path_input()
     fileout = path_output('d2.csv')
     fileres = path_results('test_rfigc_test_one_file2.csv')
-    rfigc.main('-i "%s" -d "%s" -g -f' % (filein, fileout))
-    rfigc.main('-i "%s" -d "%s" -f' % (filein, fileout))
+    rfigc.main('-i "%s" -d "%s" -g -f --silent' % (filein, fileout))
+    rfigc.main('-i "%s" -d "%s" -f --silent' % (filein, fileout))
     assert check_eq_files(fileout, fileres)
