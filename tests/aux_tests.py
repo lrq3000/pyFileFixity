a436cb2f7e0287454de613c85c7b82bdcd2db5ba
Stephen L <lrq3000@gmail.com>
2015-11-12 16:57:33 +0100

    fix unit test equality checks
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- tests/aux_tests.py
+++ tests/aux_tests.py
@@ -1,18 +1,51 @@
 import os
-import filecmp
 
-def check_eq_files(path1, path2):
+def check_eq_files(path1, path2, blocksize=65535):
     """ Return True if both files are identical, False otherwise """
-    return filecmp.cmp(path1, path2, shallow=False)
+    flag = True
+    with open(path1, 'rb') as f1, open(path2, 'rb') as f2:
+        buf1 = 1
+        buf2 = 1
+        while 1:
+            buf1 = f1.read(blocksize)
+            buf2 = f2.read(blocksize)
+            if buf1 != buf2 or (buf1 and not buf2) or (buf2 and not buf1):
+                # Reached end of file or the content is different, then return false
+                flag = False
+                break
+            elif (not buf1 and not buf2):
+                # End of file for both files
+                break
+    return flag
+    #return filecmp.cmp(path1, path2, shallow=False)  # does not work on Travis
 
 def check_eq_dir(path1, path2):
-    """ Return True if both folders have totally identical files, False otherwise """
-    d = filecmp.dircmp(path1, path2)
-    a = d.left_only + d.right_only + d.diff_files + d.funny_files
-    if a:
+    """ Return True if both folders have same structure totally identical files, False otherwise """
+    # List files in both directories
+    files1 = []
+    files2 = []
+    for root, dirs, files in os.walk(path1):
+        files1.extend(files)
+    for root, dirs, files in os.walk(path2):
+        files2.extend(files)
+    # Ensure the same order for both lists (filesystem can spit the files in whatever order it wants)
+    files1.sort()
+    files2.sort()
+
+    # Different files in one or both lists: we fail
+    if files1 != files2:
         return False
+    # Else we need to compare the files contents
     else:
-        return True
+        flag = True
+        for i in xrange(len(files1)):
+            #print("files: %s %s" % (files1[i], files2[i]))  # debug
+            # If the files contents are different, we fail
+            if not check_eq_files(os.path.join(path1, files1[i]), os.path.join(path2, files2[i])):
+                flag = False
+                break
+        # Else if all files contents were equal and all files are in both lists, success!
+        return flag
 
 def fullpath(relpath):
     '''Relative path to absolute'''
