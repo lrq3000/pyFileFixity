f62e981f7c78f532ebcc8a2c0b8ae2345eeda2aa
Stephen L <lrq3000@gmail.com>
2015-12-09 19:51:05 +0100

    tee compat py3
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- pyFileFixity/lib/tee.py
+++ pyFileFixity/lib/tee.py
@@ -3,15 +3,17 @@
 
 import sys
 
+from ._compat import b
+
 class Tee(object):
     """ Redirect print output to the terminal as well as in a log file """
 
-    def __init__(self, name=None, mode=None, nostdout=False):
+    def __init__(self, name=None, mode=None, nostdout=False, silent=False):
         self.file = None
         self.nostdout = nostdout
+        self.silent = silent
         if not nostdout:
-            self.__del__.im_func.stdout = sys.stdout
-            self.stdout = self.__del__.im_func.stdout # The weakref proxy is to prevent Python, or yourself from deleting the self.files variable somehow (if it is deleted, then it will not affect the original file list). If it is not the case that this is being deleted even though there are more references to the variable, then you can remove the proxy encapsulation. http://stackoverflow.com/questions/865115/how-do-i-correctly-clean-up-a-python-object
+            self.stdout = sys.stdout
             sys.stdout = self
         if name is not None and mode is not None:
             self.filename = name
@@ -21,26 +23,34 @@ class Tee(object):
     def __del__(self):
         """ Restore stdout and close file when Tee is closed """
         self.flush() # commit all latest changes before exiting
-        if not self.nostdout and hasattr(self.__del__.im_func, 'stdout'): sys.stdout = self.__del__.im_func.stdout
+        if not self.nostdout and hasattr(self, 'stdout'):
+            sys.stdout = self.stdout
+            self.stdout = None
         if self.file: self.file.close()
 
     def write(self, data, end="\n", flush=True):
         """ Output data to stdout and/or file """
-        if not self.nostdout:
-            self.stdout.write(data)
-            self.stdout.write(end.encode())
-        if self.file is not None:
-            self.file.write(data)
-            self.file.write(end.encode())
-        if flush:
-            self.flush()
+        if not self.silent:
+            if not self.nostdout:
+                self.stdout.write(data)
+                self.stdout.write(end)
+            if self.file is not None:
+                # Binary mode: need to convert to byte objects if Python 3
+                if 'b' in self.filemode:
+                    data = b(data)
+                    end = b(end)
+                self.file.write(data)
+                self.file.write(end)
+            if flush:
+                self.flush()
 
     def flush(self):
         """ Force commit changes to the file and stdout """
-        if not self.nostdout:
-            self.stdout.flush()
-        if self.file is not None:
-            self.file.flush()
+        if not self.silent:
+            if not self.nostdout:
+                self.stdout.flush()
+            if self.file is not None:
+                self.file.flush()
 
     # def disable(self):
         # """ Temporarily disable Tee's redirection """
@@ -55,8 +65,8 @@ class Tee(object):
     # def enable(self):
         # """ Reenable Tee's redirection after being temporarily disabled """
         # if not self.nostdout and not self.stdout:
-            # self.__del__.im_func.stdout = sys.stdout
-            # self.stdout = self.__del__.im_func.stdout # The weakref proxy is to prevent Python, or yourself from deleting the self.files variable somehow (if it is deleted, then it will not affect the original file list). If it is not the case that this is being deleted even though there are more references to the variable, then you can remove the proxy encapsulation. http://stackoverflow.com/questions/865115/how-do-i-correctly-clean-up-a-python-object
+            # self.__del__.stdout = sys.stdout
+            # self.stdout = self.__del__.stdout # The weakref proxy is to prevent Python, or yourself from deleting the self.files variable somehow (if it is deleted, then it will not affect the original file list). If it is not the case that this is being deleted even though there are more references to the variable, then you can remove the proxy encapsulation. http://stackoverflow.com/questions/865115/how-do-i-correctly-clean-up-a-python-object
             # sys.stdout = self
         # if not self.file and self.filename is not None and self.filemode is not None:
             # self.file = open(self.filename, self.filemode)
