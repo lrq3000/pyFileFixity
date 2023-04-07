0d58f51d9e04f91beccb78152361027e9a551dab
Stephen L <lrq3000@gmail.com>
2023-04-07 17:13:43 +0200

    build: fix inclusion of tests files by restoring MANIFEST.in + exclude profilers + restate support for Py2.7 in trove classifiers + fix setup.cfg trove classifiers syntax
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- pyFileFixity/__init__.py
+++ pyFileFixity/__init__.py
@@ -1,16 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#import os, json
-
-#version_file = os.path.join(os.path.dirname(__file__), '_infos.json')
-#with open(version_file, 'r') as f:
-#    _infos = json.load(f)
-
-#__author__ = _infos['author']
-#__email__ = _infos['email']
-#__version__ = _infos['version']
 __author__ = "Stephen Karl Larroque",
 __email__ = "LRQ3000@gmail.com",
 __version__ = "3.0.9b1"
 __all__ = ['__author__', '__email__', '__version__']
-
