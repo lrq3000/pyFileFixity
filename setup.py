17ab40252b0facba29fceb2b87cb84cbe2a8d17f
Stephen L <lrq3000@gmail.com>
2023-05-07 17:04:39 +0200

    fix: setuptools complaining that there are two parallel bundling config files
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- setup.py
+++ setup.py
@@ -1,1 +1,4 @@
 # An empty setup.py is required for retrocompatibility with older versions of pip that do not support pyproject.toml-only projects, especially to install in editable mode, see: https://github.com/pypa/setuptools/issues/2816
+
+from setuptools import setup
+setup()  # necessary to have at least a setup() call, otherwise setuptools will complaint with an exception: `AssertionError: Multiple .egg-info directories found\nerror: subprocess-exited-with-error`
